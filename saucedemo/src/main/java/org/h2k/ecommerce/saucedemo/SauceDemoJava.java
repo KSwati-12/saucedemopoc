c5aca1f846a9e0a5d192e4fe6b74420597b4f44e
skhos <skhos@DESKTOP-J9KJPQ8>
2024-12-25 17:33:13 -0600

    added more taste cases

--- saucedemo/src/main/java/org/h2k/ecommerce/saucedemo/SauceDemoJava.java
+++ saucedemo/src/main/java/org/h2k/ecommerce/saucedemo/SauceDemoJava.java
@@ -23,7 +23,7 @@ public class SauceDemoJava {
 		public void loginSauceDemo()
 		{
 			driver.findElement( By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
-		      
+		       
 		    driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		        
 		    driver.findElement(By.id("login-button")).click(); 
